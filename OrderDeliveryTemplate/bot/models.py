04f0ede78115fda1087d3efb7abdf72bc391caf5
Dightmerc <dightmerc@gmail.com>
2020-02-05 22:02:20 +0500

    feat: bot: added models: Message, PasSystem, Settings, TelegramUser

--- OrderDeliveryTemplate/bot/models.py
+++ OrderDeliveryTemplate/bot/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+
+class TelegramUser(models.Model):
+    telegramId = models.PositiveIntegerField("ID Telegram")
+
+    phone = models.PositiveIntegerField("Номер телефона")
+    username = models.CharField("Username")
+
+    def __str__(self):
+        return f"{self.id} {self.telegramId}"
+
+
+class Message(models.Model):
+    title = models.CharField("Название")
+    number = models.PositiveIntegerField("Порядковый номер")
+
+    ru = models.TextField("Текст на русском")
+    uz = models.TextField("Текст на узбекском")
+
+    def __str__(self):
+        return f"{self.id} {self.title}"
+
+
+class PaySystem(models.Model):
+    title = models.CharField("Название")
+    
+    token = models.CharField("Токен")
+
+    def __str__(self):
+        return f"{self.id} {self.title}"
+
+
+class Settings(models.Model):
+    title = models.CharField("Название")
+
+    token = models.CharField("Токен бота")
+    paySystem = models.ManyToMany(PaySystem)
+
+    def __str__(self):
+        return f"{self.id} {self.title}"
